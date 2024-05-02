8738285385a0753e87c07f393c9a7cbdc216abb6
mustit-hyjang <94949361+mustit-hyjang@users.noreply.github.com>
2024-05-02 11:40:43 +0900

    ADD: scss

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <main>
+      <slot></slot>
+    </main>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,16 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  typescript:{
-    shim:false,
-    typeCheck:true
+  typescript: {
+    shim: false,
+    typeCheck: true,
   },
 
-  modules: ["@nuxtjs/storybook"],
+  modules: ['@nuxtjs/storybook'],
   storybook: {
     url: 'http://localhost:6006',
     storybookRoute: '/__storybook__',
     port: 6006,
   },
-})
+  css: ['@/assets/scss/service.scss'],
+});
