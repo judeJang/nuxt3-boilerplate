486084b40408a567130fe095c072bae239cb0f15
mustit-hyjang <94949361+mustit-hyjang@users.noreply.github.com>
2024-05-02 10:55:20 +0900

    Nuxt & TS

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  typescript:{
+    shim:false,
+    typeCheck:true
+  },
+
+  modules: ["@nuxtjs/storybook"],
+  storybook: {
+    url: 'http://localhost:6006',
+    storybookRoute: '/__storybook__',
+    port: 6006,
+  },
+})
